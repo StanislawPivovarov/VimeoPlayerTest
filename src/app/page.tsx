43ee092999a0fc5d032643d663537986c0dda5bd
Стас Пивоваров <stas.povovarov@headmade.pro>
2024-10-21 18:14:16 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,35 @@
-import Image from "next/image";
+"use client"
+import { VimeoPlayer } from "reactjs-vimeo-player";
 import styles from "./page.module.css";
+import 'reactjs-vimeo-player/dist/style.css'
+import { Button, Modal } from "antd";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const [open, setIsOpen] = useState(false)
+  const [open1, setIsOpen1] = useState(false)
+  return (
+    <>
+
+    <Button onClick={() => setIsOpen(true)}>Открыть модалку (видео 4:3)</Button>
+    <Button onClick={() => setIsOpen1(true)}>Открыть модалку (видео 16:9)</Button>
+    <Modal open={open}
+    width={1280}
+    style={{top: 10}}
+    destroyOnClose
+    onCancel={() => setIsOpen(false)}
+    >
+      <VimeoPlayer  fullWidth className={styles.player} id={1021748228}/>
+    </Modal>
+    <Modal open={open1}
+    width={1280}
+    style={{top: 10}}
+    destroyOnClose
+    onCancel={() => setIsOpen1(false)}
+    >
+      <VimeoPlayer  fullWidth className={styles.player} id={1021766325}/>
+    </Modal>
+    </>
   );
 }
